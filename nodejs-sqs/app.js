6cb3d39ae11c78056ff8de44cc95b4dcc51444ed
thao.vu <thaovu@miu.edu>
2025-03-02 15:25:59 -0600

    added receive sqs message route

--- nodejs-sqs/app.js
+++ nodejs-sqs/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require('express');
-const { SQSClient, SendMessageCommand } = require('@aws-sdk/client-sqs'); // AWS SDK v3
+const { SQSClient, SendMessageCommand, ReceiveMessageCommand, DeleteMessageCommand } = require('@aws-sdk/client-sqs'); // AWS SDK v3
 
 const {REGION, SQS_URL} = process.env;
 
@@ -36,6 +36,43 @@ app.post('/send-message', async (req, res) => {
   } catch (err) {
     console.error('Error sending message:', err);
     return res.status(500).json({ error: 'Error sending message to SQS' });
+  }
+});
+
+
+
+// Route to receive message from SQS
+app.get('/receive-message', async (req, res) => {
+  try {
+    const params = {
+      QueueUrl: SQS_URL,
+      MaxNumberOfMessages: 1,  // Max number of messages to retrieve (up to 10)
+      WaitTimeSeconds: 20,     // Long polling for up to 20 seconds (to reduce cost)
+      VisibilityTimeout: 30   // How long to hide the message from other consumers (in seconds)
+    };
+
+    const data = await sqsClient.send(new ReceiveMessageCommand(params));
+    
+    if (data.Messages && data.Messages.length > 0) {
+      const messageBody = data.Messages[0].Body;
+      console.log('Received message:', messageBody);
+
+      // After processing, delete the message from the queue to prevent it from being received again
+      const deleteParams = {
+        QueueUrl: params.QueueUrl,
+        ReceiptHandle: data.Messages[0].ReceiptHandle,
+      };
+      await sqsClient.send(new DeleteMessageCommand(deleteParams));
+      console.log('Message deleted from the queue');
+      
+      // Send the message back in the response
+      return res.status(200).json({ message: messageBody });
+    } else {
+      return res.status(404).json({ message: 'No messages received' });
+    }
+  } catch (err) {
+    console.error('Error receiving message:', err);
+    return res.status(500).json({ error: 'Error receiving message from SQS' });
   }
 });
 
